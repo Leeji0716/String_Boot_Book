cda3df1f99c2b30954526a205dd6a105679888e3
Dumbbell04 <Dumbbelloper04@gmail.com>
2024-04-03 15:10:50 +0900

    origin

---
+++ jpa_string_boot_2/src/main/java/com/example/jpa_string_boot_2/JpaStringBoot2Application.java
@@ -0,0 +1,13 @@
+package com.example.jpa_string_boot_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaStringBoot2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaStringBoot2Application.class, args);
+	}
+
+}
